3c7bfd3cbbc1e398b29798fdc8de5fb9b1c455dc
Lucas Montanari <lmonty2011@gmail.com>
2020-11-02 18:49:44 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -6,8 +6,20 @@ def nyc_pigeon_organizer(data)
   # go through the outer most hash, keys are description values are hashes
   data.each { |k,v| 
     # go through the secondary value hash, keys are description values are arrays of names
-    v.each { |k2, name| 
-    # 
+    v.each { |k2, names| 
+      # go through each name in names array
+      names.each { |name| 
+        # if name is not in new hash add it, add values to new hash
+        if new_data[name] == nil 
+          new_data[name] =  {
+            color: []
+            gender: []
+            lives: []
+          }
+        end
+        new_data[name][k] << k2
+      }
     }
   }
+  new_data
 end
