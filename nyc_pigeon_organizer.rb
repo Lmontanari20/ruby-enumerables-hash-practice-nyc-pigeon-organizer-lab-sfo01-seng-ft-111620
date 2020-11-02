7d069cb261281cf40e7429cc1e7f290ce8394fd7
Lucas Montanari <lmonty2011@gmail.com>
2020-11-02 18:52:59 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -9,14 +9,16 @@ def nyc_pigeon_organizer(data)
     v.each { |k2, names| 
       # go through each name in names array
       names.each { |name| 
-        # if name is not in new hash add it, add values to new hash
+        # if name is not in new hash add it with corresponding key
         if new_data[name] == nil 
           new_data[name] =  {
-            color: [],
-            gender: [],
-            lives: []
+            k => []
           }
         end
+        # if the name is there but the corresponding key isn't add it and make it an array
+        if new_data[name][k] == nil {
+          new_data[name][k] = []
+        }
         new_data[name][k] << k2.to_s
       }
     }
