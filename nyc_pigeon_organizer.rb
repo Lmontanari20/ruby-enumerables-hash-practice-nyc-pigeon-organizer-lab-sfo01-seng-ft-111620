9ab5a97e440858a48c80ae9e370841e267b72a88
Lucas Montanari <lmonty2011@gmail.com>
2020-11-02 18:41:34 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,18 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   
+  new_data = {}
+  
+  data.each { |k,v| 
+    v.each { |k2, name| 
+      if new_data[name]
+        if new_data[name][k]
+          new_data[name][k] << k2
+        else 
+          new_data[name][k] = [k]
+      else
+        
+      end
+    }
+  }
 end
