dec42b8152adecffac0385bf13964d826889ce75
Lucas Montanari <lmonty2011@gmail.com>
2020-11-02 18:44:18 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,16 +3,11 @@ def nyc_pigeon_organizer(data)
   
   new_data = {}
   
+  # go through the outer most hash, keys are description values are hashes
   data.each { |k,v| 
+    # go through the secondary value hash, keys are description values are arrays of names
     v.each { |k2, name| 
-      if new_data[name]
-        if new_data[name][k]
-          new_data[name][k] << k2
-        else 
-          new_data[name][k] = [k]
-      else
-        
-      end
+    # 
     }
   }
 end
